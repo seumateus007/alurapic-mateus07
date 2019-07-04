f155e43c6b7e002fbcb61f1487f5d1e07d9dea9f
seumateus007 <mateus.costa@fncit.com.br>
2019-07-04 16:49:14 -0300

    [MSC] ALURAPIC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   title = 'alurapic';
+  description = 'computador';
+  url = 'https://s3.amazonaws.com/uploads.hotmart.com/blog/2018/08/BLOG_Imagens-Google_como-pesquisar-no-Google-usando-imagens.png    '
 }
